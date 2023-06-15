ed48b3347d30d9ba1f72db374f4f137441e0375e
rupnik <rok.rupnik@rls.si>
2023-06-15 10:07:32 +0200

    Reverted dark mode setting in tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   presets: [],
-  darkMode: 'class', // or 'class'
+  darkMode: 'media', // or 'class'
   theme: {
     screens: {
       sm: '640px',
